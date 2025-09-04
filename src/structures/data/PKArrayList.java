330d68938758587f95ae33cdc950f8188a4d2785
iPriyadarshi <priyadarshi1505@gmail.com>
2025-09-05 00:12:21 +0530

    refactored PKArrayList

--- src/structures/data/PKArrayList.java
+++ src/structures/data/PKArrayList.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class MyArrayList {
+public class PKArrayList {
     public static void main(String[] args) {
         // Creation
 
